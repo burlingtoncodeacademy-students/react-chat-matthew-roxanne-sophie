13907c782ebabb9d31993103e0910fce701688ac
Matthew Crownover <matthew.j.crownover@gmail.com>
2021-11-15 11:30:34 -0500

    Setting up socket.io in room.jsx and the server.js. Making minor changes to input maxLength and other minor tweaks.

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "../styles/main.css";
 import NavBar from "./NavBar";
+import { io } from "socket.io-client";
 
 /*-------------------------Function for Room */
 function Room() {
@@ -14,6 +15,50 @@ function Room() {
   //setting up state variables
   const [messages, setMessages] = useState([]);
   const [currentRoom, setCurrentRoom] = useState(roomNameCorrect);
+
+  // const socket = io("http://localhost:8000");
+  // socket.on("connect", () => {
+  //   console.log(`You connected with id: ${socket.id}`);
+  // });
+
+  // socket.on("receive-message", (message) => {
+  //   let correctedRoomName = roomName.toLowerCase();
+  //   fetch(`/allmessages/${correctedRoomName}`)
+  //     .then((res) => res.json())
+  //     .then((chatArray) => {
+  //       setMessages(chatArray);
+  //       console.log(chatArray);
+  //     });
+  // });
+
+  //useEffect attempt to limit socket.io firing multiple times. This works on multiple fires on the front end, but still firing x4 on the server side.
+  const socket = io("http://localhost:8000");
+  useEffect(() => {
+    //.on watching for connect event and console logging in the browser the generated socket id
+    socket.on("connect", () => {
+      console.log(`You connected with id: ${socket.id}`);
+    });
+
+    //.on watching for the receive-message evt that is triggered in the back end server
+    socket.on("receive-message", (message) => {
+      //re-fetching the messages and using the state variable setting to trigger the components to rerender in the return
+      let correctedRoomName = roomName.toLowerCase();
+      fetch(`/allmessages/${correctedRoomName}`)
+        .then((res) => res.json())
+        .then((chatArray) => {
+          setMessages(chatArray);
+          console.log(chatArray);
+        });
+    });
+  }, []);
+
+  //evt handler for submission of a message
+  const socketSubmit = (evt) => {
+    //creating a var for the msg value from our form using dot notation
+    const message = evt.target.msg.value;
+    //.emit to send the message through the websocket, to trigger a .on in the back end
+    socket.emit("send-message", message);
+  };
 
   // This useEffect fires on URL-change (which happens when a user navigates to a chat room). It populates previous messages for that room.
   useEffect(() => {
@@ -28,18 +73,18 @@ function Room() {
   }, [roomName]); // END useEffect
 
   // This useEffect is initiated on URL-change, but due to setInterval, the fetch is delayed by 10 seconds due to the setInterval, then data is fetched every 10 seconds.
-  useEffect(() => {
-    let correctedRoomName = roomName.toLowerCase();
-    const interval = setInterval(() => {
-      fetch(`/allmessages/${correctedRoomName}`)
-        .then((res) => res.json())
-        .then((chatArray) => {
-          setMessages(chatArray);
-          console.log(chatArray);
-        });
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [roomName]);
+  // useEffect(() => {
+  //   let correctedRoomName = roomName.toLowerCase();
+  //   const interval = setInterval(() => {
+  //     fetch(`/allmessages/${correctedRoomName}`)
+  //       .then((res) => res.json())
+  //       .then((chatArray) => {
+  //         setMessages(chatArray);
+  //         console.log(chatArray);
+  //       });
+  //   }, 5000);
+  //   return () => clearInterval(interval);
+  // }, [roomName]);
 
   //generating submit path for form based on the roomName const variable
   let submitPath = `/submit/${roomName.toLowerCase()}`;
@@ -62,7 +107,7 @@ function Room() {
             } else {
               return (
                 <div className="odd-message">
-                  {msg.author}: {msg.msg}
+                  {msg.author} : {msg.msg}
                 </div>
               );
             }
@@ -72,7 +117,7 @@ function Room() {
         <div class-name="submit-message">
           <h3>Submit a Message</h3>
           <h4>You are signed in as: {username}</h4>
-          <form method="POST" action={submitPath}>
+          <form method="POST" action={submitPath} onSubmit={socketSubmit}>
             <input type="hidden" name="author" value={username} />
             <label for="msg">Message:</label>
             <input
@@ -80,15 +125,16 @@ function Room() {
               name="msg"
               placeholder="Enter a message"
               className="userInput"
+              maxLength="200"
             />
             <input type="submit" value="Submit" className="submitButton" />
           </form>
         </div>
         <div className="room-item">
           <NavBar username={username} setCurrentRoom={setCurrentRoom} />
-          </div>
-          {/* Link back to homepage */}
-          <div className="room-item">
+        </div>
+        {/* Link back to homepage */}
+        <div className="room-item">
           <a href="/">Back to Home Page</a>
         </div>
       </div>
